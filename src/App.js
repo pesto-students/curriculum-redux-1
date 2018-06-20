5872eb231b4d7065e64642e5a58318e49bec49ed
Nikhil Arora <nikhilarora@maplegraph.com>
2018-06-21 05:06:42 +0530

    WIP: reducers partially done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-const App = () => (
-  <div>App</div>
-);
+class App extends Component {
+  state = {
+    input: '',
+  };
+
+  onChangeHandler = () => {
+
+  }
+
+  onClickHandler = () => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.input} onChange={this.onChangeHandler} />
+        <input type="button" value="Add" onClick={this.onClickHandler} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+// const store = createStore(rootReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
